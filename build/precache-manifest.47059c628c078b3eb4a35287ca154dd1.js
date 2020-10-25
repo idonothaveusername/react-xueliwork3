e7c940d1bb31edea6f1398e8fd5dd9ad44b0d08f
idonothaveusername <2428320639@qq.com>
2020-10-25 12:44:56 +0800

    work3

---
+++ build/precache-manifest.47059c628c078b3eb4a35287ca154dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6743e6e7717c0ceb4a8a3015e540f25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94590f3b56509414a67",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "74b3d05cf8de67a0c836",
+    "url": "/static/css/main.0a49d272.chunk.css"
+  },
+  {
+    "revision": "a94590f3b56509414a67",
+    "url": "/static/js/2.c80f8537.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.c80f8537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b3d05cf8de67a0c836",
+    "url": "/static/js/main.fb226078.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
